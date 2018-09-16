eb3b7619638f592b8e675b2595b2f4bcda3dc9ea
Corey Walsh <matthias61984@hotmail.com>
2018-09-15 21:54:07 -0700

    needs gameEnd and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,48 +4,53 @@ $(document).ready(function() {
 // Define question and answer combinations as objects
     var q1 = {
         question: "question number 1?",
-        answers: {
-            wAnswer1: "Wrong",
-            wAnswer2: "Wrong",
-            wAnswer3: "Wrong",
-            rAnswer: "Right"
-        },
+        cQuestion: "Right",
+        answers: [
+            "Wrong",
+            "Wrong",
+            "Wrong",
+            "Right"
+        ]
     };
     var q2 = {
         question: "question number 2?",
-        answers: {
-            wAnswer1: "Wrong",
-            wAnswer2: "Wrong",
-            wAnswer3: "Wrong",
-            rAnswer: "Right"
-        },
+        cQuestion: "Right",
+        answers: [
+            "Wrong",
+            "Wrong",
+            "Wrong",
+            "Right"
+        ]
     };
     var q3 = {
         question: "question number 3?",
-        answers: {
-            wAnswer1: "Wrong",
-            wAnswer2: "Wrong",
-            wAnswer3: "Wrong",
-            rAnswer: "Right"
-        },
-    }; 
+        cQuestion: "Right",
+        answers: [
+            "Wrong",
+            "Wrong",
+            "Wrong",
+            "Right"
+        ]
+    };
     var q4 = {
         question: "question number 4?",
-        answers: {
-            wAnswer1: "Wrong",
-            wAnswer2: "Wrong",
-            wAnswer3: "Wrong",
-            rAnswer: "Right"
-        },
+        cQuestion: "Right",
+        answers: [
+            "Wrong",
+            "Wrong",
+            "Wrong",
+            "Right"
+        ]
     };
     var q5 = {
         question: "question number 5?",
-        answers: {
-            wAnswer1: "Wrong",
-            wAnswer2: "Wrong",
-            wAnswer3: "Wrong",
-            rAnswer: "Right"
-        },
+        cQuestion: "Right",
+        answers: [
+            "Wrong",
+            "Wrong",
+            "Wrong",
+            "Right"
+        ]
     };
 // Place all the question objects into an array for indexing
     var qArray = [q1, q2, q3, q4, q5];
@@ -67,21 +72,25 @@ $(document).ready(function() {
 // Start the game up
     function startGame() {
         var index = 0;
+        var currentQ = qArray[index];
         buildQuestion();
 // Loop through object to present each question
     function buildQuestion() {
+    // Reset timer to 15
         timeLeft = 15;
-        index++;
+    // Shuffle the order of the div elements in the answer row
+        shuffle (currentQ.answers);
+        console.log(qArray);
         if (index < qArray.length) {
             var newQueDiv = $("<div>").text(qArray[index].question);
             $("#questionDiv").html(newQueDiv);
-            var newAnsDiv1 = $("<div>").text(qArray[index].answers.wAnswer1).addClass("answer incorrect");
+            var newAnsDiv1 = $("<div>").text(currentQ.answers[0]).addClass("answer");
             $("#answersDiv1").html(newAnsDiv1);
-            var newAnsDiv2 = $("<div>").text(qArray[index].answers.wAnswer2).addClass("answer incorrect");
+            var newAnsDiv2 = $("<div>").text(currentQ.answers[1]).addClass("answer");
             $("#answersDiv2").html(newAnsDiv2);
-            var newAnsDiv3 = $("<div>").text(qArray[index].answers.wAnswer3).addClass("answer incorrect");
+            var newAnsDiv3 = $("<div>").text(currentQ.answers[2]).addClass("answer");
             $("#answersDiv3").html(newAnsDiv3);
-            var newAnsDiv4 = $("<div>").text(qArray[index].answers.rAnswer).addClass("answer");
+            var newAnsDiv4 = $("<div>").text(currentQ.answers[3]).addClass("answer");
             $("#answersDiv4").html(newAnsDiv4);
         } else {
             console.log("Game over!");
@@ -89,36 +98,39 @@ $(document).ready(function() {
             $("#timerDiv").text("Time left: " + timeLeft);
             clearInterval(timer);
         }
-    // Shuffle the order of the div elements in the answer row
-    
+        index++;
     // On click functions for answering questions
         $(".answer").click(function() {
-        // If incorrect choice is chosen
-            if ($(this).hasClass("incorrect")) {
-            // Increment incorrectly answered question 
-                incorrectQ++;
-                console.log("Wrong!");
-            } else {
+            var choice = $(this).text();
+        // Check clicked choice against correct answer
+            if (choice === currentQ.cQuestion) {
             // Increment correctly answered question
                 correctQ++;
                 console.log("Correct!");
+            } else {
+            // Increment incorrectly answered question
+                incorrectQ++;
+                console.log("Wrong!");
             };
             buildQuestion();
+            if (index > qArray.length) {
+                $(".answer").off("click");
+            }
         });
+    };
+    // Set timer equal to 15 seconds and...
+        var timeLeft = 15;
+    // ...define the countdown timer function
+        var timer = setInterval(function() {
+            $("#timerDiv").text("Time left: " + timeLeft);
+            timeLeft--;
+            if (timeLeft < 0) {
+                incorrectQ++;
+                console.log("Timeout!");
+                buildQuestion();
+            }
+        }, 1000);
     }
-// Set timer equal to 15 seconds and...
-    var timeLeft = 15;
-// ...define the countdown timer function
-    var timer = setInterval(function() {
-        $("#timerDiv").text("Time left: " + timeLeft);
-        timeLeft--;
-        if (timeLeft < 0) {
-            incorrectQ++;
-            console.log("Timeout!");
-            buildQuestion();
-        }
-    }, 1000);
-}
 // Call start game function on start button click
     $("#startBtn").click(function() {
         $(this).hide();
